6bb8551362493c82056ba681bca73fe56b33ccc3
kumaran85 <mkumaran.anand@gmail.com>
2019-11-14 12:29:31 +0530

    Used in Organization

--- cb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -r carbonblack /home/ec2-user/
-chmod u+x /home/ec2-user/carbonblack/CarbonBlackClientSetup-linux-v6.1.3.71227.sh
-sh /home/ec2-user/carbonblack/CarbonBlackClientSetup-linux-v6.1.3.71227.sh
-exit
